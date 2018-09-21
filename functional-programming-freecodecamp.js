13f8e2804ca33564aed9d5b0b0cbae5e36fc1763
Dan Lubbers <lubbers.dan@gmail.com>
2018-09-21 11:35:30 -0400

    finished lessons 3-5

--- functional-programming-freecodecamp.js
+++ functional-programming-freecodecamp.js
@@ -88,3 +88,75 @@ const tea4BlackTeamFCC = getTea1(prepareBlackTea, 13); // :(
 //   tea4BlackTeamFCC
 // );
 
+// 3. Understand the Hazards of Using Imperative Code
+
+// tabs is an array of titles of each site open within the window
+var Window = function(tabs) {
+  this.tabs = tabs; // we keep a record of the array inside the object
+};
+
+// When you join two windows into one window
+Window.prototype.join = function (otherWindow) {
+  this.tabs = this.tabs.concat(otherWindow.tabs);
+  return this;
+};
+
+// When you open a new tab at the end
+Window.prototype.tabOpen = function (tab) {
+  this.tabs.push('new tab'); // let's open a new tab for now
+  return this;
+};
+
+// When you close a tab
+Window.prototype.tabClose = function (index) {
+  var tabsBeforeIndex = this.tabs.splice(0, index); // get the tabs before the tab
+  var tabsAfterIndex = this.tabs.splice(index); // get the tabs after the tab
+
+  this.tabs = tabsBeforeIndex.concat(tabsAfterIndex); // join them together 
+  return this;
+ };
+
+// Let's create three browser windows
+var workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']); // Your mailbox, drive, and other work sites
+var socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']); // Social sites
+var videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']); //  Entertainment sites
+
+// Now perform the tab opening, closing, and other operations
+var finalTabs = socialWindow
+                    .tabOpen() // Open a new tab for cat memes
+                    .join(videoWindow.tabClose(2)) // Close third tab in video window, and join
+                    .join(workWindow.tabClose(1).tabOpen());
+
+// alert(finalTabs.tabs);
+
+// 4. Avoid Mutations and Side Effects Using Functional Programming
+
+// the global variable
+var fixedValue = 4;
+
+function incrementer () {
+  // Add your code below this line
+  return fixedValue + 1;
+  
+  // Add your code above this line
+}
+
+var newValue = incrementer(); // Should equal 5
+console.log(fixedValue); // Should print 4
+console.log(newValue);
+
+// 5. Pass Arguements to Avoid External Dependence in a Function
+
+// the global variable
+var fixedValue1 = 4;
+
+// Add your code below this line
+function incrementer1 (param) {
+  return param + 1
+  
+  // Add your code above this line
+}
+
+var newValue1 = incrementer1(fixedValue1); // Should equal 5
+console.log(fixedValue1); // Should print 4
+console.log(newValue1)
